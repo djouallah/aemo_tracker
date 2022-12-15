527224af1f24bb4d9b70ea03e019cf33152fd6c0
Mimoune <djouallah@users.noreply.github.com>
2022-12-15 12:24:17 +1000

    Update app.py

--- app.py
+++ app.py
@@ -13,9 +13,9 @@ st.title("Example of Cloudflare R2 and DuckDB")
 
 col1, col2 = st.columns([1, 1])
 
-########################################################## Query arrow table as an ordinary SQL Table#####################################
+########################################################## import Data from R2##############################
 @st.experimental_singleton
-def define_view():
+def import_data(ttl=5*60):
     con=duckdb.connect()
     con.execute(f'''
     install httpfs;
@@ -27,30 +27,27 @@ def define_view():
     set s3_secret_access_key = '{st.secrets["aws_secret_access_key_secret"] }';
     set s3_endpoint = '{st.secrets["endpoint_url_secret"].replace("https://", "")}'  ;
     SET s3_url_style='path';
-    create or replace view scada as select  *  from parquet_scan('s3://delta/aemo/scada/data/*/*.parquet' , HIVE_PARTITIONING = 1,filename= 1) ;
+    create or replace table scada as Select SETTLEMENTDATE, (SETTLEMENTDATE - INTERVAL 10 HOUR) as LOCALDATE ,
+                      DUID,MIN(SCADAVALUE) as mwh from  parquet_scan('s3://delta/aemo/scada/data/*/*.parquet' , HIVE_PARTITIONING = 1,filename= 1) group by all  ;
     ''')
     return con
-con=define_view()
-@st.experimental_memo (ttl=5*60)
-def get_data():
-  return con.execute('''
-     with xx as (Select SETTLEMENTDATE, (SETTLEMENTDATE - INTERVAL 10 HOUR) as LOCALDATE , DUID,MIN(SCADAVALUE) as mwh from  scada group by all)
-     Select SETTLEMENTDATE,LOCALDATE, sum(mwh) as mwh from  xx group by all order by SETTLEMENTDATE desc
-      ''').df() 
-results = get_data()
-now = str(results["SETTLEMENTDATE"].max())
-st.subheader("Latest Updated: " + now)
 
-############################################################# Visualisation ##############################################################
-#localdate is just a stuid hack, Javascript read datetime as UTC not local time :(
+########################################################## Query the Data #####################################
+con=import_data()
+
+results= con.execute(''' Select SETTLEMENTDATE,LOCALDATE, sum(mwh) as mwh from  scada group by all order by SETTLEMENTDATE desc''').df() 
+
+st.subheader("Latest Updated: " + str(results["SETTLEMENTDATE"].max()))
+
+############################################################# Visualisation ####################################
+#localdate is just a stupid hack, Javascript read datetime as UTC not local time :(
 import altair as alt
 c = alt.Chart(results).mark_area().encode( x='LOCALDATE:T', y='mwh:Q',
                                           tooltip=['LOCALDATE','mwh']).properties(
                                             width=1200,
                                             height=400)
 st.write(c)
-#df=results[['SETTLEMENTDATE','mwh']]
-#st.dataframe(df)
+
 
 ###########################################################Buttons and Links #############################################################
 #Download Button
@@ -60,7 +57,7 @@ def convert_df(df):
      # IMPORTANT: Cache the conversion to prevent computation on every rerun
     return df.to_csv().encode('utf-8')
 
-csv = convert_df(results)
+csv = convert_df(results[['SETTLEMENTDATE','mwh']])
 col2.download_button(
      label="Download data as CSV",
      data=csv,
