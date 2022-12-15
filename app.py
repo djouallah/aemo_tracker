ddf7ea275513d529d8310517332af86644e751bb
Mimoune <djouallah@users.noreply.github.com>
2022-12-15 14:04:56 +1000

    Update app.py

--- app.py
+++ app.py
@@ -16,8 +16,8 @@ st.title("Example of Cloudflare R2 and DuckDB")
 col1, col2 = st.columns([1, 1])
 
 ########################################################## import Data from R2##############################
-@st.experimental_singleton
-def import_data(ttl=5*60):
+@st.experimental_singleton(ttl=5*60)
+def import_data():
     con=duckdb.connect()
     con.execute(f'''
     install httpfs;
