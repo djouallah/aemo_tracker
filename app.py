b3ec0c11557bc9542fadeac652a1735b4177c2d1
Mimoune <djouallah@users.noreply.github.com>
2022-12-18 16:03:29 +1000

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,6 @@ col1, col2 = st.columns([1, 1])
 ########################################################## import Data from R2##############################
 @st.experimental_singleton(ttl=5*60)
 def import_data():
-    #Date={cut_off}
     cut_off=datetime.strftime(datetime.now(pytz.timezone('Australia/Brisbane')), '%Y-%m-%d')
     con=duckdb.connect()
     con.execute(f'''
@@ -34,7 +33,7 @@ def import_data():
     set s3_endpoint = '{st.secrets["endpoint_url_secret"].replace("https://", "")}'  ;
     SET s3_url_style='path';
     create or replace table scada as Select SETTLEMENTDATE, (SETTLEMENTDATE - INTERVAL 10 HOUR) as LOCALDATE ,
-         DUID,MIN(SCADAVALUE) as mw from  parquet_scan('s3://delta/aemo/scada/data/*/*.parquet' , HIVE_PARTITIONING = 1)
+         DUID,MIN(SCADAVALUE) as mw from  parquet_scan('s3://delta/aemo/scada/data/Date={cut_off}/*.parquet' , HIVE_PARTITIONING = 1)
          group by all order by DUID,SETTLEMENTDATE
     ''')
     return con
