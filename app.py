571869fbdd245c85e6736d6d9e778b95c49cbb0b
Mimoune <djouallah@users.noreply.github.com>
2023-04-03 09:23:16 +1000

    Update app.py

--- app.py
+++ app.py
@@ -42,7 +42,13 @@ con = import_data()
 
 try :
     
-    DUID_Select= st.sidebar.multiselect('Select Station', con.sql(''' Select distinct stationame from  station order by stationame ''').df() )
+    DUID_Select= st.sidebar.multiselect('Select Station', con.sql(''' Select distinct stationame from  station
+                                                                    inner join station
+                                                                    on scada.DUID = station.DUID
+                                                                    where SCADAVALUE !=0
+                                                                    order by stationame
+                                                                    ''').df()
+                                                                    )
 
     xxxx = "','".join(DUID_Select)
     filter =  "'"+xxxx+"'"
