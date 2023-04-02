c2b074cac8d1043496ba2ed27c8690c5900a34f2
Mimoune <djouallah@users.noreply.github.com>
2023-04-02 21:13:24 +1000

    Update app.py

--- app.py
+++ app.py
@@ -34,9 +34,9 @@ def import_data(x):
 
 ########################################################## Query the Data #####################################
 
-station = import_data('''Select DUID,min(Region) as Region,	min(FuelSourceDescriptor) as FuelSourceDescriptor ,
+station = import_data("""Select DUID,min(Region) as Region,	min(FuelSourceDescriptor) as FuelSourceDescriptor ,
                                     replace(min(stationame), '''', '') as stationame, min(DispatchType) as DispatchType
-                                    from  parquet_scan('s3://aemo/aemo/duid/duid.parquet' ) group by all ;''')
+                                    from  parquet_scan('s3://aemo/aemo/duid/duid.parquet' ) group by all """)
 scada=import_data(f"""
       Select SETTLEMENTDATE, xx.DUID,Region,FuelSourceDescriptor, ,MIN(SCADAVALUE) as mw
             from  parquet_scan('s3://aemo/aemo/scada/data/*/*.parquet' , HIVE_PARTITIONING = 1)
