d0440fecd3580e45fe45358ae8efb3f088c65b4e
Mimoune <djouallah@users.noreply.github.com>
2023-04-02 14:43:58 +1000

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ st.title("Australian Electricity Market")
 
 col1, col2 = st.columns([1, 1])
 
-@st.cache_resource(ttl=5*60)
+#@st.cache_resource(ttl=5*60)
 def import_data():
    s3_file_system = s3fs.S3FileSystem(
          key=  st.secrets["aws_access_key_id_secret"],
