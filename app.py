cbf9848b7645f168e18f8cf2b48f1d600ba47875
Mimoune <djouallah@users.noreply.github.com>
2023-04-02 22:06:16 +1000

    Update app.py

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ try :
                             on scada.DUID = station.DUID
                             group by all
                             ''').df() 
-        c = alt.Chart(results).mark_area().encode( x=alt.X('day', axis=alt.Axis(title="")), y='mwh:Q',color='FuelSourceDescriptor:N',
+        c = alt.Chart(results).mark_bar().encode( x=alt.X('day', axis=alt.Axis(title="")), y='mwh:Q',color='FuelSourceDescriptor:N',
                                                 tooltip=['day','FuelSourceDescriptor','mwh']).properties(
                                                     width=1200,
                                                     height=400)
