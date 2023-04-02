4d69021e0623fc7c944c108d7ad42f80abf2625d
Mimoune <djouallah@users.noreply.github.com>
2023-04-02 17:52:28 +1000

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def import_data():
          client_kwargs={
             'endpoint_url': st.secrets["endpoint_url_secret"] 
          },
-       use_listings_cache=False
+       default_cache_type=""
       )
    fs = WholeFileCacheFileSystem(fs=s3_file_system,cache_storage="./cache", check_files= True)
    duckdb.register_filesystem(s3_file_system)
