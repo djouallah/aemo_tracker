356e187d71552e896641fb1e50fd1d3bc1c9df66
Mimoune <djouallah@users.noreply.github.com>
2023-04-29 00:19:54 +1000

    Update app.py

--- app.py
+++ app.py
@@ -79,7 +79,7 @@ try :
                                                     height=400)
     max= con.sql('''select strftime(max(SETTLEMENTDATE), '%A, %-d %B %Y - %I:%M:%S %p') as max from scada''').fetchone()
     st.write(max)
-    st.subheader("Latest Updated: " + str(max[['test']].values[0][0]))
+    #st.subheader("Latest Updated: " + str(max[['test']].values[0][0]))
 
     ############################################################# Visualisation ####################################
     #localdate is just a stupid hack, Javascript read datetime as UTC not local time :(
