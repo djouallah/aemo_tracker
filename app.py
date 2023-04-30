d2fc0f415b214158fa238cc179f05444f51ea3e1
Mimoune <djouallah@users.noreply.github.com>
2023-04-30 13:30:17 +1000

    Update app.py

--- app.py
+++ app.py
@@ -67,11 +67,11 @@ try :
                                                 height=400)
         
     else:
-        if max_day <= 2 :
+        if max_day <= 7 :
             details='minutes'
         else:
             details='hour'
-        results= con.sql(f''' Select date_trunc('{details}',(SETTLEMENTDATE - INTERVAL 10 HOUR)) as UTC,date_trunc('hour',SETTLEMENTDATE) as SETTLEMENTDATE,
+        results= con.sql(f''' Select date_trunc('{details}',(SETTLEMENTDATE - INTERVAL 10 HOUR)) as UTC,date_trunc('{details}',SETTLEMENTDATE) as SETTLEMENTDATE,
                             FuelSourceDescriptor,sum(mw)/12 as mwh from  scada
                             inner join station
                             on scada.DUID = station.DUID
