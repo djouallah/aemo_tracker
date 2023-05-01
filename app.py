49ba224e6c311dd8a28260c0f7b162e8a7f237fc
Mimoune <djouallah@users.noreply.github.com>
2023-05-01 23:45:33 +1000

    Update app.py

--- app.py
+++ app.py
@@ -71,7 +71,7 @@ try :
                                                    y='mwh:Q',
                                                    color='FuelSourceDescriptor:N',
                                                   opacity=alt.condition(selection, alt.value(1), alt.value(0)),
-                                                  tooltip=[alt.Tooltip("UTC:T", format="%Y-%b-%d %H"), 'FuelSourceDescriptor','mwh']).properties(
+                                                  tooltip=[alt.Tooltip("UTC:T", format="%Y-%b-%d %H %p"), 'FuelSourceDescriptor','mwh']).properties(
                                                     width=1200,
                                                     height=400).add_selection(
                                                              selection
