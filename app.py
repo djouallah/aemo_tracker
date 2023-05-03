e7b81886d20542cd4e61da33ed43b8618fcf7e9d
Mimoune <djouallah@users.noreply.github.com>
2023-05-03 22:10:31 +1000

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from datetime import datetime , timedelta
 import duckdb 
 import pytz
 import altair as alt
-import os
 import time
 st.set_page_config(
     page_title="Australian Electricity",
@@ -12,32 +11,43 @@ st.set_page_config(
 st.title("Australian Electricity Market")
 col1, col2 = st.columns([1, 1])
 now = datetime.now(pytz.timezone('Australia/Brisbane'))
-################################## Data import from Cloudflare R2###########################################
-@st.cache_resource(ttl=5*60)
+################################## Data import from Cloudflare R2#########################
+@st.cache_resource(ttl=5*60) 
 def import_data():
   con=duckdb.connect('db')
   con.sql(f'''
-          install httpfs;LOAD httpfs;SET enable_http_metadata_cache=true ;
-          set s3_region = 'auto';
-          set s3_access_key_id = "{st.secrets["aws_access_key_id_secret"]}" ;
-          set s3_secret_access_key = '{st.secrets["aws_secret_access_key_secret"] }';
-          set s3_endpoint = '{st.secrets["endpoint_url_secret"]}'  
-          ''')
+        install httpfs; LOAD httpfs; set enable_progress_bar=false;
+        PRAGMA enable_object_cache; SET enable_http_metadata_cache=true ;
+        set s3_region = 'auto';
+        set s3_access_key_id = "{st.secrets["aws_access_key_id_secret"]}" ;
+        set s3_secret_access_key = '{st.secrets["aws_secret_access_key_secret"] }';
+        set s3_endpoint = '{st.secrets["endpoint_url_secret"].replace("https://", "")}'  ;
+        SET s3_url_style='path';
+        ''')
+  dt = con.sql(f''' select distinct filename from  parquet_scan('s3://aemo/aemo/scada/data/*/*.parquet',filename=1)''').df()
+  con.sql(''' CREATE TABLE IF NOT EXISTS scada(filename VARCHAR, SETTLEMENTDATE TIMESTAMP, DUID VARCHAR, mw  DOUBLE  ''')
   con.sql(""" create or replace table station as 
             Select DUID,min(Region) as Region,	min(trim(FuelSourceDescriptor)) as FuelSourceDescriptor ,
             replace(min(stationame), '''', '') as stationame, min(DispatchType) as DispatchType
             from  parquet_scan('s3://aemo/aemo/duid/*.parquet' ) group by all
                           """)
-  con.sql("""create or replace table scada as Select SETTLEMENTDATE, DUID, MIN(SCADAVALUE) as mw, mw/12 as mwh
-            from  parquet_scan('s3://aemo/aemo/scada/data/*/*.parquet')  group by all  
-                  """)
+  
+  delta=dt['filename'].to_list()
+  duck=con.sql(''' select distinct filename from scada ''').df()
+  duck=duck['filename'].to_list()
+  files_to_insert = list(set(delta) - set(duck))
+  files_to_delete = list(set(duck)-set(delta))
+  if len(files_to_insert) != 0 :
+    con.execute(f''' insert into scada  Select filename ,SETTLEMENTDATE, DUID,MIN(SCADAVALUE) as mw  from  parquet_scan({files_to_insert},filename=1) group by all  ''')
+  if len(files_to_delete) != 0 :
+   for i in files_to_delete :
+    con.execute(f''' delete from scada where filename = '{i}' ''')
   return con
-########################################################## Query the Data ###################################
-max_day = st.slider('Filter days', 0, 60, 7)
+########################################################## Query the Data ########################
+max_day = st.slider('Filter days', 0, 1, 60)
 con=duckdb.connect('db')
 try :
-    station_list = con.sql(''' Select distinct stationame from  station
-                               order by stationame''').df()
+    station_list = con.sql(''' Select distinct stationame from  station order by stationame''').df()
     DUID_Select= st.multiselect('Select Station', station_list  )
 
     xxxx = "','".join(DUID_Select)
@@ -57,7 +67,7 @@ try :
         
     else:
         results= con.sql(f''' Select date_trunc('hour',(SETTLEMENTDATE - INTERVAL 10 HOUR)) as date,date_trunc('hour',SETTLEMENTDATE) as SETTLEMENTDATE,
-                            FuelSourceDescriptor,sum(mwh) as mwh from  scada
+                            FuelSourceDescriptor,sum(mw)/12 as mwh from  scada
                             inner join station
                             on scada.DUID = station.DUID
                             where SETTLEMENTDATE >= '{datetime.strftime(now - timedelta(days=max_day), '%Y-%m-%d')}'
@@ -75,6 +85,7 @@ try :
                                                              selection
                                                       )
     max= con.sql('''select strftime(max(SETTLEMENTDATE), '%A, %-d %B %Y - %I:%M:%S %p') as max from scada''').df()
+    #st.write(max)
     st.write("Latest Updated: " + str(max[['max']].values[0][0]))
 
     ############################################################# Visualisation ####################################
@@ -100,4 +111,4 @@ try :
      st.write(con.sql('select count(*) as total_records from scada').df())
 except:
     st.write('first run will take time')
-    con =import_data()
+    con = import_data()
