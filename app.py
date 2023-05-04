e2c508531b294d14a47cb0ec78befccc997314d9
Mimoune <djouallah@users.noreply.github.com>
2023-05-04 22:42:15 +1000

    Move from Hive to Delta

--- app.py
+++ app.py
@@ -3,15 +3,17 @@ from datetime import datetime , timedelta
 import duckdb 
 import pytz
 import altair as alt
+from deltalake import DeltaTable
 import time
 st.set_page_config(
-    page_title="Australian Electricity",
-    page_icon="✅"
+    page_title="POC Building a delta lake using Python and DuckDB",
+    page_icon="✅" ,
+    layout="wide"
 )
-st.title("Australian Electricity Market")
+st.title("Australian Electricity Market, Building a Data Pipeline using Delta Lake and DuckDB")
 col1, col2 = st.columns([1, 1])
 now = datetime.now(pytz.timezone('Australia/Brisbane'))
-################################## start DB #########################
+################################## generate DB#########################
 @st.cache_resource(ttl=24*60*60) 
 def build_DB():
   con=duckdb.connect('db')
@@ -21,37 +23,63 @@ def build_DB():
         set s3_region = 'auto';
         set s3_access_key_id = "{st.secrets["aws_access_key_id_secret"]}" ;
         set s3_secret_access_key = '{st.secrets["aws_secret_access_key_secret"] }';
-        set s3_endpoint = '{st.secrets["endpoint_url_secret"]}'  ;
+        set s3_endpoint = '{st.secrets["endpoint_url_secret"].replace("https://", "")}'  ;
         SET s3_url_style='path';
         ''')
-  
-  con.sql(''' CREATE TABLE IF NOT EXISTS 
-         scada(filename VARCHAR, SETTLEMENTDATE TIMESTAMP NOT NULL, DUID VARCHAR, mw  DOUBLE  )
-         ''')
+  con.sql(''' CREATE TABLE IF NOT EXISTS scada(filename VARCHAR, SETTLEMENTDATE TIMESTAMP, DUID VARCHAR, mw  DOUBLE, PRIMARY KEY (SETTLEMENTDATE, DUID) )  ''')
   con.sql(""" create or replace table station as 
             Select DUID,min(Region) as Region,	min(trim(FuelSourceDescriptor)) as FuelSourceDescriptor ,
             replace(min(stationame), '''', '') as stationame, min(DispatchType) as DispatchType
             from  parquet_scan('s3://aemo/aemo/duid/*.parquet' ) group by all
                           """)
   return con
-################################## Data import from Cloudflare R2#############################
+################################## Data import from Cloudflare R2#########################
 @st.cache_resource(ttl=5*60) 
 def import_data():
-  dt = con.sql(f''' select distinct filename from  parquet_scan('s3://aemo/aemo/scada/data/*/*.parquet',filename=1)''').df()
-  delta=dt['filename'].to_list()
+  delta_path = 's3://delta/scada'
+  storage_options = {
+  "Region": "us-east-1",   
+  "AWS_ACCESS_KEY_ID":  st.secrets["aws_access_key_id_secret"],
+  "AWS_SECRET_ACCESS_KEY": st.secrets["aws_secret_access_key_secret"]   ,   
+  "AWS_ENDPOINT_URL" : st.secrets["endpoint_url_secret"] ,
+  "AWS_S3_ALLOW_UNSAFE_RENAME":"true"
+        }
+  ###  dt.files() return the list of parquet files, from the delta log
+  #    without an expensive directory list, that's pretty much the core idea here
+  start = time.time()
+  dt = DeltaTable(delta_path,storage_options=storage_options)
+  filelist= dt.files()
+  stop = time.time()
+  duration = round(stop-start,2)
+  with st.expander("General Stats"):
+   st.write('Delta Lake file listing duration (Second): '+str(duration))
+   st.write('Total Nbr of files: '+str(len(filelist)))
+   total = con.execute(''' Select count(*) as total from scada ''').df()
+   st.write("Total records " + str(total[['total']].values[0][0]))
+  delta = [delta_path +"/" + i for i in filelist]
   duck=con.sql(''' select distinct filename from scada ''').df()
   duck=duck['filename'].to_list()
   files_to_insert = list(set(delta) - set(duck))
   files_to_delete = list(set(duck)-set(delta))
-  if len(files_to_insert) != 0 :
-    con.execute(f''' insert into scada  Select filename ,SETTLEMENTDATE, DUID,MIN(SCADAVALUE) as mw  from  parquet_scan({files_to_insert},filename=1) group by all  ''')
   if len(files_to_delete) != 0 :
    for i in files_to_delete :
     con.execute(f''' delete from scada where filename = '{i}' ''')
+  if len(files_to_insert) != 0 :
+    start = time.time()
+    insert = con.execute(f''' insert into scada  Select filename ,SETTLEMENTDATE, DUID,MIN(SCADAVALUE) as mw
+    from  parquet_scan({files_to_insert},filename=1) group by all  ''').df()
+    stop = time.time()
+    duration = round(stop-start,2)
+    with st.expander("Data inserted"):
+     st.write('Reading from R2 and Inserting into DuckDB (Second): '+str(duration))
+     st.write('Total Nbr of files: '+str(len(files_to_insert)))
+     st.write("nbr of records inserted " + str(insert[['Count']].values[0][0]))
+  with st.expander("Delta Lake Metadata"):
+   st.dataframe(dt.get_add_actions(flatten=True).to_pandas(),use_container_width=True)
   return con
 ########################################################## Query the Data ########################
-con=duckdb.connect('db')
 max_day = st.slider('Filter days', 0, 1, 60)
+con=duckdb.connect('db')
 try :
     station_list = con.sql(''' Select distinct stationame from  station order by stationame''').df()
     DUID_Select= st.multiselect('Select Station', station_list  )
@@ -68,7 +96,7 @@ try :
                             ''').df() 
         c = alt.Chart(results).mark_area().encode(x=alt.X('date:T', axis=alt.Axis(title="")), y='mw:Q',color='stationame:N',
                                             tooltip=[alt.Tooltip("date:T", format="%Y-%b-%d %I:%M%p"), 'stationame','mw']).properties(
-                                                width=1200,
+                                                width=1600,
                                                 height=400)
         
     else:
@@ -86,7 +114,7 @@ try :
                                                    color='FuelSourceDescriptor:N',
                                                   opacity=alt.condition(selection, alt.value(1), alt.value(0)),
                                                   tooltip=[alt.Tooltip("date:T", format="%Y-%b-%d %I%p"), 'FuelSourceDescriptor','mwh']).properties(
-                                                    width=1200,
+                                                    width=1600,
                                                     height=400).add_selection(
                                                              selection
                                                       )
@@ -108,16 +136,9 @@ try :
     
     link='[for a Full experience go to Nemtracker Dashboard](https://datastudio.google.com/reporting/1Fah7mn1X9itiFAMIvCFkj_tEYXHdxAll/page/TyK1)'
     col1.markdown(link,unsafe_allow_html=True)
-    start = time.time()
-    con=build_DB()
+    con = build_DB()
     con = import_data()
-    stop = time.time()
-    duration = round(stop-start,2)
-    if duration > 1 :
-      st.write('total import duration: '+str(duration))
-      xx=con.sql('select count(*) as total_records from scada').df()
-      st.write('total records :' +str( xx[['total_records']].values[0][0]))
 except:
     st.write('first run will take time')
-    con=build_DB()
+    con = build_DB()
     con = import_data()
