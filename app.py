80b888d29514342cad9146be3a567754d2294c56
Mimoune <djouallah@users.noreply.github.com>
2023-07-02 15:41:48 +1000

    Update app.py

--- app.py
+++ app.py
@@ -57,6 +57,7 @@ def import_data():
    st.write('Total Nbr of files: '+str(len(filelist)))
    total = con.execute(''' Select count(*) as total from scada ''').df()
    st.write("Total records " + str(format(total[['total']].values[0][0],",")))
+   st.write(dt.vaccum())
   delta = [delta_path +"/" + i for i in filelist]
   duck=con.sql(''' select distinct filename from scada ''').df()
   duck=duck['filename'].to_list()
