8e999614eba5f9409ebb61cacb6b177bb7301e38
Mimoune <djouallah@users.noreply.github.com>
2023-07-23 17:16:52 +1000

    run compact when you reach a threshold

--- writedelta.py
+++ writedelta.py
@@ -10,12 +10,11 @@ from pyarrow import fs
 import pyarrow.parquet as pq
 from deltalake.writer import write_deltalake
 from deltalake import DeltaTable
-delta_path = 's3://delta/scada'
+delta_path = 's3://aemo/scada'
 region="us-east-1"
 access_key = os.environ.get("aws_access_key_id_secret")
 secret_key=os.environ.get("aws_secret_access_key_secret")
 endpoint=os.environ.get("endpoint_url_secret")
-
 storage_options = {
 "Region": region,   
 "AWS_ACCESS_KEY_ID":     access_key ,
@@ -23,7 +22,6 @@ storage_options = {
 "AWS_ENDPOINT_URL" :     endpoint ,
 "AWS_S3_ALLOW_UNSAFE_RENAME":"true"
 }
-
 def get_file_path(filename):
     return os.path.join(tempfile.gettempdir(), filename)
 def load(request):   
@@ -43,7 +41,7 @@ def load(request):
     filelist_sorted=sorted(filelist_unique, reverse=True)
     filelist = filelist_sorted[:1000]
     try:
-        df = ds.dataset("delta/log/scada/log.parquet",filesystem=s3).to_table().to_pandas()
+        df = ds.dataset("aemo/log/scada/log.parquet",filesystem=s3).to_table().to_pandas()
     except:
         df=pd.DataFrame(columns=['file'])     
     file_loaded= df['file'].unique()
@@ -63,10 +61,11 @@ def load(request):
             df=df.dropna(how='all') #drop na
             df['SETTLEMENTDATE']= pd.to_datetime(df['SETTLEMENTDATE'])
             df['Date'] = df['SETTLEMENTDATE'].dt.date
+            df['week'] = df['SETTLEMENTDATE'].dt.strftime('%Y%U')
             df['file'] = x
             appended_data.append(df)
             # see pd.concat documentation for more info
-      appended_data = pd.concat(appended_data)
+      appended_data = pd.concat(appended_data,ignore_index=True)
       existing_file = pd.DataFrame( file_loaded)
       new_file = pd.DataFrame(  appended_data['file'].unique())
       log = pd.concat ([new_file,existing_file], ignore_index=True)
@@ -84,11 +83,17 @@ def load(request):
                       pa.field('DUID', pa.string()),
                       pa.field('SCADAVALUE', pa.float64()),
                       pa.field('Date', pa.date32()),
+                      pa.field('week', pa.string()),
                       pa.field('file', pa.string())
                       ]
                                                        )
       xx=tb.cast(target_schema=my_schema)
-      write_deltalake(delta_path, xx,mode='append',partition_by=['Date'],storage_options=storage_options)
-      pq.write_table(log_tb,"delta/log/scada/log.parquet",filesystem=s3)
-      DeltaTable(delta_path,storage_options=storage_options).create_checkpoint()
+      write_deltalake(delta_path, xx,mode='append',partition_by=['week'],storage_options=storage_options)
+      pq.write_table(log_tb,"aemo/log/scada/log.parquet",filesystem=s3)
+      ########################## Compact and delete old Copies ##########################
+      dt = DeltaTable(delta_path,storage_options=storage_options) 
+      if len(dt.file_uris(partition_filters = [("week","=",appended_data.at[0,'week'])])) > 20 :
+            dt.create_checkpoint()
+            dt.optimize.compact()
+            dt.vacuum(retention_hours=0,dry_run=False,  enforce_retention_duration=False)
       return "done"
